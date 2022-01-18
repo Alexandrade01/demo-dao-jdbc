54ad00f2eff715a82cdfbd80511069651b96b6ef
Alexandrade01 <solidwinter96@outlook.com>
2022-01-18 20:42:12 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,25 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-
-		Department obj = new Department(1,"books");
-		System.out.println(obj);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Department obj = new Department(1, "books");
+		Seller seller = null;
+		try {
+			seller = new Seller(1, "Joao Bosco", "jb@hotmail.com", sdf.parse("25/06/2020"), 1800.0, obj);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(seller);
 
 	}
 
